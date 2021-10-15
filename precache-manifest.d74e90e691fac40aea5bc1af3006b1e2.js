baf4e6dfd1da4eb95dabcf39338d500b1514c852
baldezzzzh <pidenkodmitriy@gmail.com>
2021-10-15 12:36:19 +0300

    Updates

---
+++ precache-manifest.d74e90e691fac40aea5bc1af3006b1e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0973a3e3238e9ed526b65cd65cdb08a1",
+    "url": "/react-hw-ig/index.html"
+  },
+  {
+    "revision": "0ccf05aa588002214b74",
+    "url": "/react-hw-ig/static/css/main.9a9f3469.chunk.css"
+  },
+  {
+    "revision": "285691c4eaa6d37d21de",
+    "url": "/react-hw-ig/static/js/2.85e2dba4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-hw-ig/static/js/2.85e2dba4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ccf05aa588002214b74",
+    "url": "/react-hw-ig/static/js/main.bc6c3c72.chunk.js"
+  },
+  {
+    "revision": "7c0da3cb4495ab17d454",
+    "url": "/react-hw-ig/static/js/runtime-main.08abaa9c.js"
+  }
+]);
